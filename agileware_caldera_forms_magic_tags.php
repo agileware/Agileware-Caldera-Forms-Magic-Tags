2d80169940a1d60887f1c98f00cbb0fb25c1b5a8
Pengyi Zhang <pengyi@agileware.com.au>
2019-05-10 09:42:16 +1000

    update version

--- agileware_caldera_forms_magic_tags.php
+++ agileware_caldera_forms_magic_tags.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Agileware Caldera Forms Magic Tags
  * Plugin URI:        https://agileware.com.au/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.1.1
+ * Version:           1.1.2
  * Author:            Agileware
  * Author URI:        https://agileware.com.au/
  * License:           GPL-2.0+
@@ -62,7 +62,7 @@ add_action( 'admin_init', 'agileware_caldera_forms_magic_tags_child_plugin_has_p
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'AGILEWARE_CALDERA_FORMS_MAGIC_TAGS_VERSION', '1.1.1' );
+define( 'AGILEWARE_CALDERA_FORMS_MAGIC_TAGS_VERSION', '1.1.2' );
 
 /**
  * The code that runs during plugin activation.
